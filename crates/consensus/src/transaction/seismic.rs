5ade923ff2f5ee4281c97683f2c3ecbb336d8df1
phexyz <32248504+phexyz@users.noreply.github.com>
2025-02-13 13:25:15 -0500

    Fixed TypedDataRequest  (#38)

--- crates/consensus/src/transaction/seismic.rs
+++ crates/consensus/src/transaction/seismic.rs
@@ -186,14 +186,6 @@ impl From<Signed<TxSeismic>> for TypedDataRequest {
     }
 }
 
-impl TryFrom<TypedData> for TxSeismic {
-    type Error = serde_json::Error;
-
-    fn try_from(value: TypedData) -> Result<Self, Self::Error> {
-        serde_json::from_value(value.message)
-    }
-}
-
 impl RlpEcdsaTx for TxSeismic {
     const DEFAULT_TX_TYPE: u8 = { Self::tx_type() as u8 };
 
