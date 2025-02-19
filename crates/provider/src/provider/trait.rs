58c96bcecac00e85a8333594cbd6d64dd7c74e74
cdrappi <c@seismic.systems>
2025-02-19 17:02:40 -0500

    ignore mainnet test

--- crates/provider/src/provider/trait.rs
+++ crates/provider/src/provider/trait.rs
@@ -1858,6 +1858,7 @@ mod tests {
         feature = "reqwest-rustls-tls",
         feature = "reqwest-native-tls",
     ))]
+    #[ignore = "this api hit its usage limit"]
     async fn call_mainnet() {
         use alloy_network::TransactionBuilder;
         use alloy_sol_types::SolValue;
