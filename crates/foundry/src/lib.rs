b80dc36305a61f63e9a790c8c9a5379998da00b7
cdrappi <c@seismic.systems>
2025-05-29 18:42:35 -0400

    cargofmt

--- crates/foundry/src/lib.rs
+++ crates/foundry/src/lib.rs
@@ -1,21 +1,10 @@
 //! Seismic overrides to types commonly used in foundry
 //! We do this to reduce future merge conflicts
-use alloy_rpc_types_eth::{Block, Header, Transaction};
-use alloy_serde::WithOtherFields;
-use derive_more::From;
-use serde::{Deserialize, Serialize};
+mod block;
+mod transaction;
 
+pub use seismic_alloy_network::Seismic as AnyNetwork;
 pub use seismic_alloy_rpc_types::SeismicTransactionRequest as TransactionRequest;
 
-/// Seismic RPC transaction
-pub type RpcTransaction = Transaction<seismic_alloy_consensus::SeismicTxEnvelope>;
-
-/// Seismic RPC transaction with other fields
-#[derive(Clone, Debug, From, PartialEq, Eq, Deserialize, Serialize)]
-pub struct AnyRpcTransaction(pub alloy_serde::WithOtherFields<RpcTransaction>);
-
-/// Seismic RPC block with other fields
-#[derive(Clone, Debug, From, PartialEq, Eq, Deserialize, Serialize)]
-pub struct AnyRpcBlock(pub WithOtherFields<Block<RpcTransaction, Header>>);
-
-pub use seismic_alloy_network::{Seismic as AnyNetwork};
+pub use block::AnyRpcBlock;
+pub use transaction::{AnyRpcTransaction, RpcTransaction};
