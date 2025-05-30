de45ec99ddbc1fb1524e1dc761f176dcd55d6a5b
cdrappi <c@seismic.systems>
2025-05-29 20:12:35 -0400

    in the middle of fix; next step add seismic-foundry chain

--- crates/foundry/src/block.rs
+++ crates/foundry/src/block.rs
@@ -6,6 +6,8 @@ use alloy_serde::WithOtherFields;
 use derive_more::From;
 use alloy_network_primitives::{BlockResponse};
 use serde::{Deserialize, Serialize};
+use seismic_alloy_network::Seismic;
+use alloy_network::Network;
 
 /// Seismic RPC block with other fields
 #[derive(Clone, Debug, From, PartialEq, Eq, Deserialize, Serialize)]
@@ -38,8 +40,8 @@ impl AnyRpcBlock {
 }
 
 impl BlockResponse for AnyRpcBlock {
-    type Header = Header;
-    type Transaction = AnyRpcTransaction;
+    type Header = <seismic_alloy_network::Seismic as Network>::Header;
+    type Transaction = <seismic_alloy_network::Seismic as Network>::TransactionResponse;
 
     fn header(&self) -> &Self::Header {
         &self.0.inner.header
