07a48a2f761498af8b58104c2ceea02c850e7ab6
Christian Drappi <christiandrappi@gmail.com>
2025-06-02 17:36:38 -0400

    debugging

---
+++ crates/provider/src/foundry/mod.rs
@@ -0,0 +1,10 @@
+//! Seismic RPC provider for foundry
+
+use alloy_provider::{Provider, RootProvider};
+use seismic_alloy_network::foundry::SeismicFoundry;
+
+// impl Provider<SeismicFoundry> for RootProvider<SeismicFoundry> {
+//     fn root(&self) -> &RootProvider<SeismicFoundry> {
+//         self
+//     }
+// }
