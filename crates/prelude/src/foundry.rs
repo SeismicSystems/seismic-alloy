2c161419e69f7addcdf34dda13ad78c9c1265689
Christian Drappi <christiandrappi@gmail.com>
2025-06-02 18:18:12 -0400

    more exports

--- crates/prelude/src/foundry.rs
+++ crates/prelude/src/foundry.rs
@@ -4,6 +4,7 @@ pub use seismic_alloy_network::foundry::{
     envelope::SeismicFoundryTxEnvelope as AnyTxEnvelope,
     tx_request::{
         SeismicFoundryRpcTransaction as AnyRpcTransaction,
+        SeismicFoundryRpcTransaction as TransactionResponse,
         SeismicFoundryTransactionRequest as AnyTransactionRequest,
     },
     typed_tx::SeismicFoundryTypedTransaction as AnyTypedTransaction,
