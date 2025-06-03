1d463693b35487d69f83b2798a3e34700b7c33b2
Christian Drappi <christiandrappi@gmail.com>
2025-06-03 16:01:49 -0400

    more exports

--- crates/prelude/src/foundry.rs
+++ crates/prelude/src/foundry.rs
@@ -21,4 +21,5 @@ pub use seismic_alloy_consensus::SeismicTxEnvelope as TxEnvelope;
 //     SeismicFoundry,
 // };
 
+pub use seismic_alloy_consensus::SeismicTypedTransaction as TypedTransaction;
 pub use seismic_alloy_rpc_types::SeismicTransactionRequest as TransactionRequest;
