e508ec87851d658c61a60a9e38802fb5f9e3294c
Christian Drappi <christiandrappi@gmail.com>
2025-06-03 16:34:34 -0400

    fix import

--- crates/prelude/src/foundry.rs
+++ crates/prelude/src/foundry.rs
@@ -1,4 +1,5 @@
 //! Aliases to drop into foundry so we don't have to rename all the types
+use alloy_primitives::Log;
 use alloy_rpc_types_eth::TransactionReceipt;
 use alloy_serde::WithOtherFields;
 use seismic_alloy_consensus::SeismicReceiptEnvelope;
@@ -18,7 +19,9 @@ pub use seismic_alloy_network::foundry::{
 };
 pub use seismic_alloy_rpc_types::SeismicTransactionRequest as TransactionRequest;
 
+/// A transaction receipt with the SeismicReceiptEnvelope wrapped in a WithOtherFields
 pub type AnyTransactionReceipt = WithOtherFields<TransactionReceipt<SeismicReceiptEnvelope<Log>>>;
+
 // pub use seismic_alloy_network::foundry::{
 //     block::SeismicFoundryRpcBlock,
 //     envelope::SeismicFoundryTxEnvelope,
