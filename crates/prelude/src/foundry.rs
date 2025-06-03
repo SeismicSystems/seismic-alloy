b41f88378a52e0b1b716cbed59e6deba9089667f
Christian Drappi <christiandrappi@gmail.com>
2025-06-03 16:31:01 -0400

    any tx receipt

--- crates/prelude/src/foundry.rs
+++ crates/prelude/src/foundry.rs
@@ -1,4 +1,10 @@
 //! Aliases to drop into foundry so we don't have to rename all the types
+use alloy_rpc_types_eth::TransactionReceipt;
+use alloy_serde::WithOtherFields;
+use seismic_alloy_consensus::SeismicReceiptEnvelope;
+
+pub use seismic_alloy_consensus::SeismicReceiptEnvelope as AnyReceiptEnvelope;
+pub use seismic_alloy_consensus::SeismicTxEnvelope as TxEnvelope;
 pub use seismic_alloy_network::foundry::{
     block::SeismicFoundryRpcBlock as AnyRpcBlock,
     envelope::SeismicFoundryTxEnvelope as AnyTxEnvelope,
@@ -10,9 +16,9 @@ pub use seismic_alloy_network::foundry::{
     typed_tx::SeismicFoundryTypedTransaction as AnyTypedTransaction,
     SeismicFoundry as AnyNetwork,
 };
+pub use seismic_alloy_rpc_types::SeismicTransactionRequest as TransactionRequest;
 
-pub use seismic_alloy_consensus::SeismicTxEnvelope as TxEnvelope;
-
+pub type AnyTransactionReceipt = WithOtherFields<TransactionReceipt<SeismicReceiptEnvelope<Log>>>;
 // pub use seismic_alloy_network::foundry::{
 //     block::SeismicFoundryRpcBlock,
 //     envelope::SeismicFoundryTxEnvelope,
@@ -20,5 +26,3 @@ pub use seismic_alloy_consensus::SeismicTxEnvelope as TxEnvelope;
 //     typed_tx::SeismicFoundryTypedTransaction,
 //     SeismicFoundry,
 // };
-
-pub use seismic_alloy_rpc_types::SeismicTransactionRequest as TransactionRequest;
