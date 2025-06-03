2ded5a6efd5e71005ea117a594c961adea5c5b81
Spencer Solit <sol@seismic.systems>
2025-06-03 11:28:25 -0400

    added gas_price to test_send_transaction

--- crates/provider/src/provider.rs
+++ crates/provider/src/provider.rs
@@ -345,10 +345,12 @@ mod tests {
         let provider = SeismicSignedProvider::new(wallet.clone(), anvil.endpoint_url());
 
         // testing send transaction
+        let gas_price = provider.get_gas_price().await.unwrap();
         let tx = SeismicTransactionRequest::default()
             .with_input(plaintext)
             .with_kind(TxKind::Create)
-            .with_nonce(1);
+            .with_nonce(1)
+            .with_gas_price(gas_price);
 
         let contract_address = provider
             .send_transaction(tx)
