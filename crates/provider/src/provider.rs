ab96b264ec5ac96189d1056d6cf4993147dbfe39
cdrappi <c@seismic.systems>
2025-06-17 17:55:12 -0400

    seismic provider test: make sure seismic tx work

--- crates/provider/src/provider.rs
+++ crates/provider/src/provider.rs
@@ -312,6 +312,7 @@ mod tests {
     use alloy_primitives::{address, Address, Bytes, TxKind};
     use alloy_provider::{ext::AnvilApi, Provider, SendableTx};
     use alloy_signer_local::PrivateKeySigner;
+    use seismic_alloy_consensus::{SeismicReceiptEnvelope, TxSeismic};
     use seismic_alloy_network::foundry::builder::seismic_foundry_tx_builder;
 
     /// Path to local sanvil binary
@@ -413,6 +414,54 @@ mod tests {
 
         let code = provider.get_code_at(contract_address).await.unwrap();
         assert_eq!(code, ContractTestContext::get_code());
+    }
+
+    #[tokio::test]
+    async fn test_send_seismic_transaction() {
+        let plaintext = ContractTestContext::get_deploy_input_plaintext();
+        let anvil = Anvil::at(SANVIL_PATH).spawn();
+        let wallet = get_wallet(&anvil);
+        let provider =
+            SeismicSignedProvider::<SeismicFoundry>::new(wallet.clone(), anvil.endpoint_url());
+
+        // testing send transaction
+        let tx =
+            seismic_foundry_tx_builder().with_input(plaintext).with_kind(TxKind::Create).into();
+
+        let pending_tx = provider.send_transaction(tx.into()).await.unwrap();
+        let receipt = pending_tx.get_receipt().await.unwrap();
+        let contract_address = receipt.contract_address.unwrap();
+        let code = provider.get_code_at(contract_address).await.unwrap();
+        assert_eq!(code, ContractTestContext::get_code());
+
+
+        let network_pk = provider.get_tee_pubkey().await.unwrap();
+        let encryption_keypair = TxSeismicElements::get_rand_encryption_keypair();
+        let elements = TxSeismicElements::default()
+            .with_encryption_pubkey(encryption_keypair.public_key())
+            .with_encryption_nonce(TxSeismicElements::get_rand_encryption_nonce());
+
+        let tx_input = ContractTestContext::get_set_number_input_plaintext();
+        let encrypted_input = elements
+            .client_encrypt(&tx_input, &network_pk, &encryption_keypair.secret_key())
+            .unwrap();
+
+        let mut tx =
+            seismic_foundry_tx_builder().with_input(encrypted_input).with_kind(TxKind::Call(contract_address)).into();
+        tx.inner.transaction_type = Some(TxSeismic::TX_TYPE);
+        tx.seismic_elements = Some(elements);
+
+        let pending_tx = provider.send_transaction(tx.into()).await.unwrap();
+        let receipt = pending_tx.get_receipt().await.unwrap();
+
+        match receipt.inner.inner {
+            SeismicReceiptEnvelope::Seismic(r) => {
+                println!("seismic receipt: {:?}", r);
+            }
+            _ => {
+                panic!("expected seismic receipt");
+            }
+        }
     }
 
     fn get_wallet(anvil: &AnvilInstance) -> SeismicWallet<SeismicFoundry> {
