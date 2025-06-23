79a0e55e74fc3b2c512d9edd34b826e8e4810d20
Spencer Solit <sol@seismic.systems>
2025-06-23 15:42:13 -0400

    test

--- crates/provider/src/provider.rs
+++ crates/provider/src/provider.rs
@@ -309,7 +309,7 @@ mod tests {
     use seismic_alloy_network::foundry::builder::seismic_foundry_tx_builder;
 
     /// Path to the sanvil binary in the github ci runner
-    const RUNNER_SANVIL_PATH: &str = "/home/runner/.seismic/bin/sanvil";
+    const RUNNER_SANVIL_PATH: &str = "sanvil";
 
     /// Path to local sanvil binary for local testing
     const SANVIL_PATH: &str = RUNNER_SANVIL_PATH;
