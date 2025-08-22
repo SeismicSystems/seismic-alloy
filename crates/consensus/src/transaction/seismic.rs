51978c7b071a5c55515d0fae28afe2419fc2e084
Matthias Wright <matthias.s.wright@gmail.com>
2025-08-23 03:09:51 +0800

    chore: update enclave and seismic-revm versions

--- crates/consensus/src/transaction/seismic.rs
+++ crates/consensus/src/transaction/seismic.rs
@@ -13,7 +13,7 @@ use alloy_serde::WithOtherFields;
 use core::mem;
 use rand::RngCore;
 use seismic_enclave::{
-    constants, ecdh_decrypt, ecdh_encrypt, rand, rpc::SyncEnclaveApiClient, GetPurposeKeysRequest,
+    constants, ecdh_decrypt, ecdh_encrypt, rand, rpc::SyncEnclaveApiClient, keys::GetPurposeKeysRequest,
     Keypair, Nonce, PublicKey, Secp256k1, SecretKey,
 };
 use thiserror::Error;
