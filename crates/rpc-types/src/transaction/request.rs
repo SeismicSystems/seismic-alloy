7f366309559733800d5ef970724cf4f852680505
cdrappi <c@seismic.systems>
2025-09-19 13:41:44 -0400

    sfrt

--- crates/rpc-types/src/transaction/request.rs
+++ crates/rpc-types/src/transaction/request.rs
@@ -2,8 +2,8 @@ use core::fmt::Error;
 
 use alloc::vec::Vec;
 use alloy_consensus::{
-    transaction::Recovered, EthereumTxEnvelope, SignableTransaction, Signed, TxEip1559, TxEip2930,
-    TxEip4844Variant, TxEip7702, TxLegacy, TypedTransaction,
+    EthereumTxEnvelope, SignableTransaction, Signed, TxEip1559, TxEip2930, TxEip4844Variant,
+    TxEip7702, TxLegacy, TypedTransaction,
 };
 use alloy_eips::{eip7702::SignedAuthorization, Typed2718};
 use alloy_network_primitives::{TransactionBuilder4844, TransactionBuilder7702};
@@ -57,18 +57,6 @@ impl SeismicTransactionRequest {
     pub fn from_transaction<T: alloy_consensus::Transaction>(tx: T) -> Self {
         let inner = TransactionRequest::from_transaction(tx);
         Self { inner, seismic_elements: None }
-    }
-
-    /// Initializes the [`SeismicTransactionRequest`] with the provided transaction, sender and
-    /// maybe seismic_elements
-    pub fn from_recovered_seismic_envelope(tx: Recovered<SeismicTxEnvelope>) -> Self {
-        let (tx, from) = tx.into_parts();
-        let mut tx_req = Self::from_transaction(tx.clone()).from(from);
-        if let SeismicTxEnvelope::Seismic(s) = tx {
-            let (p, _, _) = s.into_parts();
-            tx_req.set_seismic_elements(p.seismic_elements);
-        }
-        tx_req
     }
 
     /// Sets the transactions type for the transactions.
