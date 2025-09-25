eff8566298177ccdc3414f39bcf45aef1d0fc9fb
Christian Drappi <c@seismic.systems>
2025-09-25 13:11:49 -0400

    Revert "SeismicTypedTransaction: add Eip4844 generic parameter for blobs" (#53)

--- crates/network/src/foundry/envelope.rs
+++ crates/network/src/foundry/envelope.rs
@@ -1,11 +1,9 @@
 //! Seismic Foundry transaction envelope, meant to mimic AnyTxEnvelope
 use alloy_consensus::{
-    error::ValueError, transaction::RlpEcdsaDecodableTx, BlobTransactionSidecar,
-    EthereumTxEnvelope, Signed, Transaction as TransactionTrait, TxEip4844Variant, TxEnvelope,
-    Typed2718,
+    error::ValueError, transaction::RlpEcdsaDecodableTx, EthereumTxEnvelope, Signed,
+    Transaction as TransactionTrait, TxEip4844Variant, TxEnvelope, Typed2718,
 };
 use alloy_eip7702::SignedAuthorization;
-use alloy_eips::eip7594::Encodable7594;
 use alloy_network::{
     eip2718::{Decodable2718, Encodable2718},
     AnyTxEnvelope, UnknownTxEnvelope,
@@ -17,35 +15,18 @@ use seismic_alloy_consensus::{
 };
 
 /// Seismic Foundry transaction envelope, meant to mimic AnyTxEnvelope
-#[derive(Debug, Clone, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[cfg_attr(
-    feature = "serde",
-    serde(bound(
-        serialize = "T: serde::Serialize",
-        deserialize = "T: serde::de::DeserializeOwned"
-    ))
-)]
-pub enum SeismicFoundryTxEnvelope<T = BlobTransactionSidecar>
-where
-    T: Clone
-        + Encodable7594
-        + std::fmt::Debug
-        + Send
-        + Sync
-        + 'static
-        + serde::Serialize
-        + serde::de::DeserializeOwned,
-{
+#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[serde(untagged)]
+pub enum SeismicFoundryTxEnvelope {
     /// An Ethereum transaction.
-    Ethereum(EthereumTxEnvelope<TxEip4844Variant<T>>),
+    Ethereum(TxEnvelope),
     /// A transaction with unknown type.
     Unknown(UnknownTxEnvelope),
     /// A Seismic transaction.
     Seismic(Signed<TxSeismic>),
 }
 
-impl SeismicFoundryTxEnvelope<BlobTransactionSidecar> {
+impl SeismicFoundryTxEnvelope {
     /// Convert to AnyTxEnvelope
     pub fn to_any_tx_envelope(&self) -> AnyTxEnvelope {
         match self {
